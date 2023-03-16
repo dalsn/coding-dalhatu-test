ec96024f74df9ac13a0ca0c359b4834f36639684
Dalhatu Njidda <dalsdnjidda@gmail.com>
2023-03-16 11:22:42 +0100

    30 mins in

--- resources/js/Components/Standards.vue
+++ resources/js/Components/Standards.vue
@@ -7,7 +7,7 @@
                     name="comments"
                     type="checkbox"
                     class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    @change="addStandard(standard)"
+                    @change="toggleStandard(standard)"
                 />
             </div>
             <div class="ml-3 text-sm leading-6">
--- resources/js/Store/modules/standards.js
+++ resources/js/Store/modules/standards.js
@@ -1,32 +1,38 @@
 const state = () => ({
-  items: []
+	items: []
 })
 
 // getters
 const getters = {
-  cartProducts: (state) => {
-    return state.items;
-  }
+	cartProducts: (state) => {
+		return state.items;
+	}
 }
 
 // actions
 const actions = {
-  addItemToStandards ({ state, commit }, standard) {
-        commit('addItemToStandards', { standard })
-    }
+	addItemToStandards ({ state, commit }, standard) {
+		commit('addItemToStandards', { standard })
+	},
+	removeItemFromStandards ({ state, commit }, index) {
+		commit('removeItemFromStandards', { index })
+	}
 }
 
 // mutations
 const mutations = {
-    addItemToStandards (state, { standard }) {
-        state.items.push(standard)
-    }
+	addItemToStandards (state, { standard }) {
+		state.items.push(standard)
+	},
+	removeItemFromStandards (state, { index }) {
+		state.item.splice(index, 1)
+	}
 }
 
 export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
+	namespaced: true,
+	state,
+	getters,
+	actions,
+	mutations
 }
